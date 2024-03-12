958980a67622c6fb12afa5d26fc908944ee0e02a
h598062 <598062@stud.hvl.no>
2024-03-12 11:43:28 +0100

    maven spring boot prosjekt

---
+++ src/main/java/no/hvl/dat109/texasholdem/Dat109ProsjektTexasHoldemApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.texasholdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat109ProsjektTexasHoldemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat109ProsjektTexasHoldemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/hvl/dat109/texasholdem/Dat109ProsjektTexasHoldemApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.texasholdem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat109ProsjektTexasHoldemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
