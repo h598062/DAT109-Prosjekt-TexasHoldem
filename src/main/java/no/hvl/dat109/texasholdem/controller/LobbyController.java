fadb13d11aab38ae3a47da39682b80065d1c2770
h602408 <602408@stud.hvl.no>
2024-03-20 13:12:19 +0100

    Create LobbyController.java
    gjordt lobbycontroller

---
+++ src/main/java/no/hvl/dat109/texasholdem/controller/LobbyController.java
@@ -0,0 +1,14 @@
+package no.hvl.dat109.texasholdem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LobbyController {
+
+    @GetMapping("/lobby")
+    public String lobby() {
+        return "lobby";
+    }
+
+}
