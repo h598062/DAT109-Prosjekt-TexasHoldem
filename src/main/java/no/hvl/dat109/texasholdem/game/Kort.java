3d59ac23ded0ff5c3caa87a3ca98b67a8bbf6c72
Storm <sangolts@gmail.com>
2024-03-20 10:32:52 +0100

    asdasd

---
+++ src/main/java/no/hvl/dat109/texasholdem/game/Kort.java
@@ -0,0 +1,9 @@
+package no.hvl.dat109.texasholdem.game;
+
+public class Kort {
+    private final static int ANTALL_KORT = 52;
+
+    public Kort() {
+
+    }
+ }
--- src/main/java/no/hvl/dat109/texasholdem/game/Spiller.java
+++ src/main/java/no/hvl/dat109/texasholdem/game/Spiller.java
@@ -1,4 +1,11 @@
 package no.hvl.dat109.texasholdem.game;
 
 public class Spiller {
+    private Kort kort;
+    private int chips;
+
+    public Spiller() {
+
+    }
+
 }
