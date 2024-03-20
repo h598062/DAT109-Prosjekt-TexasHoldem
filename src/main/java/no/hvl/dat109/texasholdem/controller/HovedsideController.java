15b107e5541bcdac3d22bf0a0405c737c110e3a3
h575711 <575711@stud.hvl.no>
2024-03-20 13:21:56 +0100

    HovedsideController

--- src/main/java/no/hvl/dat109/texasholdem/controller/HovedsideController.java
+++ src/main/java/no/hvl/dat109/texasholdem/controller/HovedsideController.java
@@ -2,10 +2,11 @@ package no.hvl.dat109.texasholdem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
-public class WebController {
+public class HovedsideController {
+
     @GetMapping("/")
     public String getIndex() {
         return "hovedside";
