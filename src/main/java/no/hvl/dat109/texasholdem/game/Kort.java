0136096ccaa05fd451da5278a7c29dc5d711e44c
dat20 <huynh123@live.no>
2024-03-20 11:10:00 +0100

    Lagde Kort klassen

--- src/main/java/no/hvl/dat109/texasholdem/game/Kort.java
+++ src/main/java/no/hvl/dat109/texasholdem/game/Kort.java
@@ -1,9 +1,33 @@
 package no.hvl.dat109.texasholdem.game;
 
+import no.hvl.dat109.texasholdem.Enums.Korttype;
+
 public class Kort {
     private final static int ANTALL_KORT = 52;
+    private Korttype korttype;
+    private int verdi;
 
     public Kort() {
 
     }
- }
+    public Kort(Korttype korttype, int verdi) {
+        this.korttype = korttype;
+        this.verdi = verdi;
+    }
+
+    public int getVerdi() {
+        return verdi;
+    }
+
+    public void setVerdi(int verdi) {
+        this.verdi = verdi;
+    }
+
+    public Korttype getKorttype() {
+        return korttype;
+    }
+
+    public void setKorttype(Korttype korttype) {
+        this.korttype = korttype;
+    }
+}
