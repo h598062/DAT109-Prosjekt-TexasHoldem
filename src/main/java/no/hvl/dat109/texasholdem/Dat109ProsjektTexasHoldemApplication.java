3a0eebb042787dd7283f5fc48be4ad1b73e7ccd8
h598062 <111509904+h598062@users.noreply.github.com>
2024-04-12 19:30:57 +0200

    lagt til configs for å få spring app til å kjøre som servlet container i tomee/tomcat server

--- src/main/java/no/hvl/dat109/texasholdem/Dat109ProsjektTexasHoldemApplication.java
+++ src/main/java/no/hvl/dat109/texasholdem/Dat109ProsjektTexasHoldemApplication.java
@@ -2,12 +2,18 @@ package no.hvl.dat109.texasholdem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Dat109ProsjektTexasHoldemApplication {
+public class Dat109ProsjektTexasHoldemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dat109ProsjektTexasHoldemApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Dat109ProsjektTexasHoldemApplication.class);
+	}
 }
