92b0cb26932aaabfef07fe4df47af652823b68b3
h598062 <111509904+h598062@users.noreply.github.com>
2024-04-15 23:14:33 +0200

    fikset noen småting med dependencies og typo i test

--- src/test/java/no/hvl/dat109/texasholdem/game/EvaluateCardsTest.java
+++ src/test/java/no/hvl/dat109/texasholdem/game/EvaluateCardsTest.java
@@ -2,123 +2,120 @@ package no.hvl.dat109.texasholdem.game;
 
 import no.hvl.dat109.texasholdem.enums.Korttype;
 import org.junit.jupiter.api.Test;
-import java.lang.reflect.Method;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class EvaluateCardsTest {
 
-    @Test
-    public void testGetHighestCard() throws Exception {
-        Hand hand = new Hand();
-        hand.addCard(new Kort(Korttype.HJERTE,10));
-        hand.addCard(new Kort(Korttype.HJERTE,2));
-        hand.addCard(new Kort(Korttype.HJERTE,14));
-        hand.addCard(new Kort(Korttype.HJERTE,5));
-        hand.addCard(new Kort(Korttype.HJERTE,8));
+	@Test
+	void testGetHighestCard() throws Exception {
+		Hand hand = new Hand();
+		hand.addCard(new Kort(Korttype.HJERTE, 10));
+		hand.addCard(new Kort(Korttype.HJERTE, 2));
+		hand.addCard(new Kort(Korttype.HJERTE, 14));
+		hand.addCard(new Kort(Korttype.HJERTE, 5));
+		hand.addCard(new Kort(Korttype.HJERTE, 8));
 
-        // Use reflection to access the private method
-        Method method = EvaluateCards.class.getDeclaredMethod("getHighestCard", Hand.class);
-        method.setAccessible(true);
+		// Use reflection to access the private method
+		Method method = EvaluateCards.class.getDeclaredMethod("getHighestCard", Hand.class);
+		method.setAccessible(true);
 
-        int highestCard = (int) method.invoke(null, hand);
+		int highestCard = (int) method.invoke(null, hand);
 
-        assertEquals(14, highestCard, "høyeste kort skal være 14 ");
-    }
+		assertEquals(14, highestCard, "høyeste kort skal være 14 ");
+	}
 
-    @Test
-    public void testGameLogic() {
-        // Create hands for two players
-        Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
+	@Test
+	void testGameLogic() {
+		// Create hands for two players
+		Hand hand1 = new Hand();
+		Hand hand2 = new Hand();
 
-        // Add five same cards (on table) to both hands
-        for (int i = 2; i <= 6; i++) {
-            hand1.addCard(new Kort(Korttype.HJERTE, i));
-            hand2.addCard(new Kort(Korttype.HJERTE, i));
-        }
+		// Add five same cards (on table) to both hands
+		for (int i = 2; i <= 6; i++) {
+			hand1.addCard(new Kort(Korttype.HJERTE, i));
+			hand2.addCard(new Kort(Korttype.HJERTE, i));
+		}
 
-        // Add two unique cards to each hand
-        hand1.addCard(new Kort(Korttype.HJERTE, 10));
-        hand1.addCard(new Kort(Korttype.HJERTE, 11));
+		// Add two unique cards to each hand
+		hand1.addCard(new Kort(Korttype.HJERTE, 10));
+		hand1.addCard(new Kort(Korttype.HJERTE, 11));
 
-        hand2.addCard(new Kort(Korttype.HJERTE, 12));
-        hand2.addCard(new Kort(Korttype.HJERTE, 13));
+		hand2.addCard(new Kort(Korttype.HJERTE, 12));
+		hand2.addCard(new Kort(Korttype.HJERTE, 13));
 
-        // Use game logic to evaluate hands
-        int result = EvaluateCards.compareHand(hand1, hand2);
+		// Use game logic to evaluate hands
+		int result = EvaluateCards.compareHand(hand1, hand2);
 
-        // Check that the result is as expected
-        assertTrue(result < 0, "Hand 2 skal være vinneren");
-    }
+		// Check that the result is as expected
+		assertTrue(result < 0, "Hand 2 skal være vinneren");
+	}
 
-    @Test
-    public void testGameLogicWithTableCards() {
-        // Create hands for two players and the table
-        Hand tableHand = new Hand();
-        Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
+	@Test
+	void testGameLogicWithTableCards() {
+		// Create hands for two players and the table
+		Hand tableHand = new Hand();
+		Hand hand1     = new Hand();
+		Hand hand2     = new Hand();
 
-        // Add five cards to the table hand
-        for (int i = 2; i <= 6; i++) {
-            tableHand.addCard(new Kort(Korttype.HJERTE, i));
-        }
+		// Add five cards to the table hand
+		for (int i = 2; i <= 6; i++) {
+			tableHand.addCard(new Kort(Korttype.HJERTE, i));
+		}
 
-        // Add two unique cards to each player's hand
-        hand1.addCard(new Kort(Korttype.HJERTE, 10));
-        hand1.addCard(new Kort(Korttype.HJERTE, 11));
+		// Add two unique cards to each player's hand
+		hand1.addCard(new Kort(Korttype.HJERTE, 10));
+		hand1.addCard(new Kort(Korttype.HJERTE, 11));
 
-        hand2.addCard(new Kort(Korttype.HJERTE, 12));
-        hand2.addCard(new Kort(Korttype.HJERTE, 13));
+		hand2.addCard(new Kort(Korttype.HJERTE, 12));
+		hand2.addCard(new Kort(Korttype.HJERTE, 13));
 
-        // Combine table cards with player's cards
-        hand1.getHand().addAll(tableHand.getHand());
-        hand2.getHand().addAll(tableHand.getHand());
+		// Combine table cards with player's cards
+		hand1.getHand().addAll(tableHand.getHand());
+		hand2.getHand().addAll(tableHand.getHand());
 
-        // Use game logic to evaluate hands
-        int result = EvaluateCards.compareHand(hand1, hand2);
+		// Use game logic to evaluate hands
+		int result = EvaluateCards.compareHand(hand1, hand2);
 
-        // Check that the result is as expected
-        assertTrue(result < 0, "Hand2 skal vinne");
-    }
+		// Check that the result is as expected
+		assertTrue(result < 0, "Hand2 skal vinne");
+	}
 
-    @Test
-    public void testHighestPairWithTableCards() {
+	@Test
+	void testHighestPairWithTableCards() {
 
-        Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
+		Hand hand1 = new Hand();
+		Hand hand2 = new Hand();
 
-        hand1.addCard(new Kort(Korttype.HJERTE, 10));
-        hand1.addCard(new Kort(Korttype.KLOVER, 10));
+		hand1.addCard(new Kort(Korttype.HJERTE, 10));
+		hand1.addCard(new Kort(Korttype.KLOVER, 10));
 
-        hand2.addCard(new Kort(Korttype.HJERTE, 11));
-        hand2.addCard(new Kort(Korttype.KLOVER, 11));
+		hand2.addCard(new Kort(Korttype.HJERTE, 11));
+		hand2.addCard(new Kort(Korttype.KLOVER, 11));
 
-        List<Kort> bordKort = new ArrayList<>();
+		List<Kort> bordKort = new ArrayList<>();
 
-        bordKort.add(new Kort(Korttype.HJERTE,3));
-        bordKort.add(new Kort(Korttype.SPAR,4));
-        bordKort.add(new Kort(Korttype.RUTER,5));
-        bordKort.add(new Kort(Korttype.RUTER,6));
-        bordKort.add(new Kort(Korttype.HJERTE,7));
+		bordKort.add(new Kort(Korttype.HJERTE, 3));
+		bordKort.add(new Kort(Korttype.SPAR, 4));
+		bordKort.add(new Kort(Korttype.RUTER, 5));
+		bordKort.add(new Kort(Korttype.RUTER, 6));
+		bordKort.add(new Kort(Korttype.HJERTE, 7));
 
-        // Add table cards to player's hands
-        hand1.getHand().addAll(bordKort);
-        hand2.getHand().addAll(bordKort);
+		// Add table cards to player's hands
+		hand1.getHand().addAll(bordKort);
+		hand2.getHand().addAll(bordKort);
 
-        // Use game logic to evaluate hands
-        int result = EvaluateCards.compareHand(hand1, hand2);
+		// Use game logic to evaluate hands
+		int result = EvaluateCards.compareHand(hand1, hand2);
 
-        // Check that the result is as expected
-        assertTrue(result < 0, "Hånd 2 skal vinne );
-    }
-
-
-
-    }
+		// Check that the result is as expected
+		assertTrue(result < 0, "Hånd 2 skal vinne");
+	}
 
 
-
-
-
+}
